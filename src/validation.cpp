362305d1eed27b378cccad7d63dab1b57a491acb
Kohei Taniguchi <kohei@chaintope.com>
2019-01-23 13:16:20 +0900

    Remove calling validation for PoW.
    If not, the daemon will fail to boot in initializing process.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1091,8 +1091,7 @@ bool ReadBlockFromDisk(CBlock& block, const CDiskBlockPos& pos, const Consensus:
     }
 
     // Check the header
-    if (!CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
-        return error("ReadBlockFromDisk: Errors in block header at %s", pos.ToString());
+    // TODO: Check a proof of Signed Blocks in a block header in here
 
     return true;
 }
@@ -3078,9 +3077,7 @@ static bool FindUndoPos(CValidationState &state, int nFile, CDiskBlockPos &pos, 
 
 static bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW = true)
 {
-    // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
-        return state.DoS(50, false, REJECT_INVALID, "high-hash", false, "proof of work failed");
+    // TODO: Check a proof of Signed Blocks in a block header in here
 
     return true;
 }
