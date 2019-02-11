0550046f93c6605d40bf324585d6c8eb813f8031
Navia Bheeman <navia@chaintope.com>
2019-02-11 11:12:46 +0800

    changed IsValidSignatureEncoding to handle signatures without hashtype byte and removed IsValidDataSignatureEncoding

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -85,8 +85,12 @@ bool static IsCompressedPubKey(const valtype &vchPubKey) {
  * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
  *
  * This function is consensus-critical since BIP66.
+ * 
+ * Parameter dataSignature added for use with OP_CHECKDATASIG
+ * In signatures that are input of OP_CHECKDATASIG 
+ * there is no <hashtype> byte
  */
-bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig) {
+bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig, bool dataSignature = false) {
     // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S] [sighash]
     // * total-length: 1-byte length descriptor of everything that follows,
     //   excluding the sighash byte.
@@ -99,15 +103,18 @@ bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig) {
     // * sighash: 1-byte value indicating what data is hashed (not part of the DER
     //   signature)
 
+    const unsigned int numparts = dataSignature ? 8 : 9;
+    const unsigned int size = dataSignature ? 72 : 73;
+
     // Minimum and maximum size constraints.
-    if (sig.size() < 9) return false;
-    if (sig.size() > 73) return false;
+    if (sig.size() < numparts) return false;
+    if (sig.size() > size) return false;
 
     // A signature is of type 0x30 (compound).
     if (sig[0] != 0x30) return false;
 
     // Make sure the length covers the entire signature.
-    if (sig[1] != sig.size() - 3) return false;
+    if (sig[1] != sig.size() - (dataSignature ? 2 : 3)) return false;
 
     // Extract the length of the R element.
     unsigned int lenR = sig[3];
@@ -120,7 +127,7 @@ bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig) {
 
     // Verify that the length of the signature matches the sum of the length
     // of the elements.
-    if ((size_t)(lenR + lenS + 7) != sig.size()) return false;
+    if ((size_t)(lenR + lenS +  (dataSignature ? 6 : 7)) != sig.size()) return false;
 
     // Check whether the R element is an integer.
     if (sig[2] != 0x02) return false;
@@ -151,125 +158,15 @@ bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig) {
     return true;
 }
 
-
-/**
- * checks OP_CHECKDATASIG data signature encoding. 
- * Note:there is no <hashtype> byte
- * 
- * Read IsValidSignatureEncoding for help and documentation
+/*
+ * Parameter dataSignature added for use with OP_CHECKDATASIG
+ * In signatures that are input of OP_CHECKDATASIG 
+ * there is no <hashtype> byte
  */
-static bool IsValidDataSignatureEncoding(const valtype &sig) {
-    // Minimum and maximum size constraints.
-    if (sig.size() < 8 || sig.size() > 72) {
-        return false;
-    }
-
-    // Check that the signature is a compound structure of proper size.
-    // A signature is of type 0x30 (compound).
-    if (sig[0] != 0x30) {
-        return false;
-    }
-
-    // Make sure the length covers the entire signature.
-    // Remove:
-    // * 1 byte for the coupound type.
-    // * 1 byte for the length of the signature.
-    if (sig[1] != sig.size() - 2) {
-        return false;
-    }
-
-    // Check that R is an positive integer of sensible size.
-    // Check whether the R element is an integer.
-    if (sig[2] != 0x02) {
-        return false;
-    }
-
-    // Extract the length of the R element.
-    const uint32_t lenR = sig[3];
-
-    // Zero-length integers are not allowed for R.
-    if (lenR == 0) {
-        return false;
-    }
-
-    // Negative numbers are not allowed for R.
-    if (sig[4] & 0x80) {
-        return false;
-    }
-
-    // Make sure the length of the R element is consistent with the signature
-    // size.
-    // Remove:
-    // * 1 byte for the coumpound type.
-    // * 1 byte for the length of the signature.
-    // * 2 bytes for the integer type of R and S.
-    // * 2 bytes for the size of R and S.
-    // * 1 byte for S itself.
-    if (lenR > (sig.size() - 7)) {
-        return false;
-    }
-
-    // Null bytes at the start of R are not allowed, unless R would otherwise be
-    // interpreted as a negative number.
-    //
-    // /!\ This check can only be performed after we checked that lenR is
-    //     consistent with the size of the signature or we risk to access out of
-    //     bound elements.
-    if (lenR > 1 && (sig[4] == 0x00) && !(sig[5] & 0x80)) {
-        return false;
-    }
-
-    // Check that S is an positive integer of sensible size.
-    // S's definition starts after R's definition:
-    // * 1 byte for the coumpound type.
-    // * 1 byte for the length of the signature.
-    // * 1 byte for the size of R.
-    // * lenR bytes for R itself.
-    // * 1 byte to get to S.
-    const uint32_t startS = lenR + 4;
-
-    // Check whether the S element is an integer.
-    if (sig[startS] != 0x02) {
-        return false;
-    }
-
-    // Extract the length of the S element.
-    const uint32_t lenS = sig[startS + 1];
-
-    // Zero-length integers are not allowed for S.
-    if (lenS == 0) {
-        return false;
-    }
-
-    // Negative numbers are not allowed for S.
-    if (sig[startS + 2] & 0x80) {
-        return false;
-    }
-
-    // Verify that the length of S is consistent with the size of the signature
-    // including metadatas:
-    // * 1 byte for the integer type of S.
-    // * 1 byte for the size of S.
-    if (size_t(startS + lenS + 2) != sig.size()) {
-        return false;
-    }
-
-    // Null bytes at the start of S are not allowed, unless S would otherwise be
-    // interpreted as a negative number.
-    //
-    // /!\ This check can only be performed after we checked that lenR and lenS
-    //     are consistent with the size of the signature or we risk to access
-    //     out of bound elements.
-    if (lenS > 1 && (sig[startS + 2] == 0x00) && !(sig[startS + 3] & 0x80)) {
-        return false;
-    }
-
-    return true;
-}
-
+  
 bool static IsLowDERSignature(const valtype &vchSig, ScriptError* serror, bool dataSignature = false) {
 
-    if (dataSignature ? !IsValidDataSignatureEncoding(vchSig) : !IsValidSignatureEncoding(vchSig)) {
+    if ( !IsValidSignatureEncoding(vchSig, dataSignature)) {
         return set_error(serror, SCRIPT_ERR_SIG_DER);
     }
     // https://bitcoin.stackexchange.com/a/12556:
@@ -304,7 +201,7 @@ bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned i
     }
 
     if ((flags & (SCRIPT_VERIFY_DERSIG | SCRIPT_VERIFY_LOW_S | SCRIPT_VERIFY_STRICTENC)) != 0 
-        && ( datasignature ? !IsValidDataSignatureEncoding(vchSig) : !IsValidSignatureEncoding(vchSig)) )
+        && !IsValidSignatureEncoding(vchSig, datasignature) )
     {
         return set_error(serror, SCRIPT_ERR_SIG_DER);
     } 
