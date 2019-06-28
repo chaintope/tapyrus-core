936d66128379ed1de382fd0bc2a2fa6cac1cc5c4
Navia Bheeman <navia@chaintope.com>
2019-06-28 17:19:59 +0800

    code cleanup - adding back failfast option, removed debug

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -496,7 +496,6 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
             for i in range(MAX_NODES):
                 for entry in os.listdir(cache_path(i)):
                     if entry not in ['wallets', 'chainstate', 'blocks', 'genesis.dat']:
-                        print(entry)
                         os.remove(cache_path(i, entry))
 
         for i in range(self.num_nodes):
