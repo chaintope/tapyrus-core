30cb015355af55f99740323eb089c629dcc06881
Pieter Wuille <pieter.wuille@gmail.com>
2019-10-29 11:55:59 -0700

    Run background seeding periodically instead of unpredictably
    * Instead of calling RandAddSeedSleep anytime the scheduler goes
      idle, call its replacement (RandAddSeedPeriodic) just once per
      minute. This has better guarantees of actually being run, and
      helps limit how frequently the dynamic env data is gathered.
    * Since this code runs once per minute regardless now, we no
      longer need to keep track of the last time strengthening was
      run; just do it always.
    * Make strengthening time context dependent (100 ms at startup,
      10 ms once per minute afterwards).

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -42,8 +42,6 @@ void CScheduler::serviceQueue()
         try {
             if (!shouldStop() && taskQueue.empty()) {
                 reverse_lock<boost::unique_lock<boost::mutex> > rlock(lock);
-                // Use this chance to get more entropy
-                RandAddSeedSleep();
             }
             while (!shouldStop() && taskQueue.empty()) {
                 // Wait until there is something to do.
