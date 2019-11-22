846d84c8bc7503bf73b74593d4087a7fcb68ac98
Navia Bheeman <navia@chaintope.com>
2019-11-22 17:05:20 +0800

    removed bip9 related comments in tests and description in rpc

--- test/functional/p2p_compactblocks.py
+++ test/functional/p2p_compactblocks.py
@@ -99,8 +99,6 @@ class CompactBlocksTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         # Node0 = pre-segwit, node1 = segwit-aware
         self.num_nodes = 2
-        # This test was written assuming SegWit is activated using BIP9 at height 432 (3x confirmation window).
-        # TODO: Rewrite this test to support SegWit being always active.
         self.extra_args = [[], ["-txindex", "-deprecatedrpc=addwitnessaddress"]]
         self.utxos = []
 
