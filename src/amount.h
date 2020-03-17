bafd968a5a330995b65bd8252cd4fb6a02f4cfdb
Winn Nie Wong <winnie@chaintope.com>
2020-03-17 10:58:13 +0800

    change satoshi to tapyrus

--- src/amount.h
+++ src/amount.h
@@ -14,7 +14,7 @@ typedef int64_t CAmount;
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
-/** No amount larger than this (in satoshi) is valid.
+/** No amount larger than this (in Tapyrus) is valid.
  *
  * Note that this constant is *not* the total money supply, which in Bitcoin
  * currently happens to be less than 21,000,000 TPC for various reasons, but
