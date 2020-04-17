a76776be7a4a0e6bc98ace47d0063cf48b0716b0
Navia Bheeman <navia@chaintope.com>
2020-04-17 18:48:44 +0530

    removed ssl code from qt gui

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -37,7 +37,6 @@
 #include <QLocalServer>
 #include <QLocalSocket>
 #include <QNetworkAccessManager>
-#include <QNetworkProxy>
 #include <QNetworkReply>
 #include <QNetworkRequest>
 #include <QSslCertificate>
