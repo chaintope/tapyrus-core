275fac0743a8185824dba0a005ae795b8cb80c6f
Navia Bheeman <navia@chaintope.com>
2020-06-25 12:13:42 +0530

    changed type byte of colorIdentifier from 0x01, 0x02, 0x03 to 0xc1, 0xc2 and 0xc3

--- src/test/coloridentifier_tests.cpp
+++ src/test/coloridentifier_tests.cpp
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_unserialize)
 
     //type REISSUABLE - insufficient data
     try {
-        CDataStream ss00(ParseHex("0100"), SER_NETWORK, INIT_PROTO_VERSION);
+        CDataStream ss00(ParseHex("c100"), SER_NETWORK, INIT_PROTO_VERSION);
         ss00 >> c0;
         BOOST_CHECK_MESSAGE(false, "We should have thrown");
     } catch (const std::ios_base::failure& e) {
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_unserialize)
 
     //type NFT - insufficient data
     try {
-        CDataStream ss00(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23f"), SER_NETWORK, INIT_PROTO_VERSION);
+        CDataStream ss00(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23f"), SER_NETWORK, INIT_PROTO_VERSION);
         ss00 >> c0;
         BOOST_CHECK_MESSAGE(false, "We should have thrown");
     } catch (const std::ios_base::failure& e) {
@@ -45,24 +45,48 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_unserialize)
     BOOST_CHECK_EQUAL(TokenToUint(c00.type), TokenToUint(TokenTypes::NONE));
     BOOST_CHECK(memcmp(&c00.payload[0], &str[0], 32) == 0);
 
-    //type unknown - 33 bytes
+    //type unknown - 33 bytes, type byte '01'
     ColorIdentifier c01;
-    CDataStream ss02(ParseHex("048282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    CDataStream ss02(ParseHex("018282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
     ss02 >> c01;
     BOOST_CHECK_EQUAL(TokenToUint(c01.type), TokenToUint(TokenTypes::NONE));
     BOOST_CHECK(memcmp(&c01.payload[0], &str[0], 32) == 0);
 
+    //type unknown - 33 bytes, type byte '02'
+    CDataStream ss03(ParseHex("028282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    ss03 >> c01;
+    BOOST_CHECK_EQUAL(TokenToUint(c01.type), TokenToUint(TokenTypes::NONE));
+    BOOST_CHECK(memcmp(&c01.payload[0], &str[0], 32) == 0);
+
+    //type unknown - 33 bytes, type byte '03'
+    CDataStream ss04(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    ss04 >> c01;
+    BOOST_CHECK_EQUAL(TokenToUint(c01.type), TokenToUint(TokenTypes::NONE));
+    BOOST_CHECK(memcmp(&c01.payload[0], &str[0], 32) == 0);
+
+    //type unknown - 33 bytes, type byte '04'
+    CDataStream ss05(ParseHex("048282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    ss05 >> c01;
+    BOOST_CHECK_EQUAL(TokenToUint(c01.type), TokenToUint(TokenTypes::NONE));
+    BOOST_CHECK(memcmp(&c01.payload[0], &str[0], 32) == 0);
+
+    //type unknown - 33 bytes
+    CDataStream ss06(ParseHex("c48282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    ss06 >> c01;
+    BOOST_CHECK_EQUAL(TokenToUint(c01.type), TokenToUint(TokenTypes::NONE));
+    BOOST_CHECK(memcmp(&c01.payload[0], &str[0], 32) == 0);
+
     //type REISSUABLE
-    std::vector<unsigned char> scriptVector(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
+    std::vector<unsigned char> scriptVector(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
     uint8_t scripthash[CSHA256::OUTPUT_SIZE];
     scriptVector.insert(scriptVector.begin(), 0x21);
     CSHA256().Write(scriptVector.data(), scriptVector.size()).Finalize(scripthash);
     
     ColorIdentifier c1;
-    CDataStream ss1(ParseHex("01f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6"), SER_NETWORK, INIT_PROTO_VERSION);
+    CDataStream ss1(ParseHex("c1f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f"), SER_NETWORK, INIT_PROTO_VERSION);
     ss1 >> c1;
-    BOOST_CHECK_EQUAL(HexStr(&scripthash[0], &scripthash[32]), "f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
-    BOOST_CHECK_EQUAL(HexStr(&c1.payload[0], &c1.payload[32]), "f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
+    BOOST_CHECK_EQUAL(HexStr(&scripthash[0], &scripthash[32]), "f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
+    BOOST_CHECK_EQUAL(HexStr(&c1.payload[0], &c1.payload[32]), "f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
     BOOST_CHECK_EQUAL(TokenToUint(c1.type), TokenToUint(TokenTypes::REISSUABLE));
     BOOST_CHECK(memcmp(&c1.payload[0], &scripthash[0], 32) == 0);
 
@@ -74,7 +98,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_unserialize)
     CSHA256().Write((const unsigned char *)s.data(), s.size()).Finalize(scripthash);
 
     ColorIdentifier c2;
-    CDataStream ss2(ParseHex("029608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d"), SER_NETWORK, INIT_PROTO_VERSION);
+    CDataStream ss2(ParseHex("c29608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d"), SER_NETWORK, INIT_PROTO_VERSION);
     ss2 >> c2;
     BOOST_CHECK_EQUAL(HexStr(&scripthash[0], &scripthash[32]), "9608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d");
     BOOST_CHECK_EQUAL(HexStr(&c2.payload[0], &c2.payload[32], false), "9608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d");
@@ -83,8 +107,8 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_unserialize)
 
     //type NFT - 33 bytes
     ColorIdentifier c04;
-    CDataStream ss03(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
-    ss03 >> c04;
+    CDataStream ss07(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"), SER_NETWORK, INIT_PROTO_VERSION);
+    ss07 >> c04;
     BOOST_CHECK_EQUAL(TokenToUint(c04.type), TokenToUint(TokenTypes::NFT));
     BOOST_CHECK_EQUAL(HexStr(&c04.payload[0], &c04.payload[32]), "8282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508");
 }
@@ -98,11 +122,11 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_serialize)
     BOOST_CHECK_EQUAL(HexStr(ss0.begin(), ss0.end(), false), "00");
 
     //type REISSUABLE
-    std::vector<unsigned char> scriptVector(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
+    std::vector<unsigned char> scriptVector(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
     ColorIdentifier c1(CScript() << scriptVector);
     CDataStream ss1(SER_NETWORK, INIT_PROTO_VERSION);
     ss1 << c1;
-    BOOST_CHECK_EQUAL(HexStr(ss1.begin(), ss1.end(), false), "01f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
+    BOOST_CHECK_EQUAL(HexStr(ss1.begin(), ss1.end(), false), "c1f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
 
     //type NON_REISSUABLE
     uint256 hashMalFix(ParseHex("485273f6703f038a234400edadb543eb44b4af5372e8b207990beebc386e7954"));
@@ -110,7 +134,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_serialize)
     ColorIdentifier c2(out, TokenTypes::NON_REISSUABLE);
     CDataStream ss2(SER_NETWORK, INIT_PROTO_VERSION);
     ss2 << c2;
-    BOOST_CHECK_EQUAL(HexStr(ss2.begin(), ss2.end()), "029608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d");
+    BOOST_CHECK_EQUAL(HexStr(ss2.begin(), ss2.end()), "c29608951ee23595caa227e7668e39f9d3525a39e9dc30d7391f138576c07be84d");
 
     //type unknown
     ColorIdentifier c4;
@@ -124,7 +148,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_valid_serialize)
 BOOST_AUTO_TEST_CASE(coloridentifier_compare)
 {
     //type REISSUABLE
-    std::vector<unsigned char> scriptVector(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
+    std::vector<unsigned char> scriptVector(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
     ColorIdentifier c1(CScript() << scriptVector);
 
     uint8_t scripthash[CSHA256::OUTPUT_SIZE];
@@ -134,9 +158,9 @@ BOOST_AUTO_TEST_CASE(coloridentifier_compare)
     c2.type = TokenTypes::REISSUABLE;
     memcpy(&c2.payload[0], &scripthash[0], 32);
 
-    BOOST_CHECK_EQUAL(HexStr(&scripthash[0], &scripthash[32]), "f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
-    BOOST_CHECK_EQUAL(HexStr(&c1.payload[0], &c1.payload[32]), "f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
-    BOOST_CHECK_EQUAL(HexStr(&c2.payload[0], &c2.payload[32]), "f55efb77e5a0e37c16d8f3484024558241c215a57aa991533152813f111482f6");
+    BOOST_CHECK_EQUAL(HexStr(&scripthash[0], &scripthash[32]), "f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
+    BOOST_CHECK_EQUAL(HexStr(&c1.payload[0], &c1.payload[32]), "f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
+    BOOST_CHECK_EQUAL(HexStr(&c2.payload[0], &c2.payload[32]), "f335bd3240ddfd87a2c2fc5a53210606460f19143f5e475729c46e06fcc9858f");
 
     BOOST_CHECK(c1.operator==(c2));
 
@@ -178,7 +202,7 @@ BOOST_AUTO_TEST_CASE(coloridentifier_compare)
 BOOST_AUTO_TEST_CASE(coloridentifier_map_compare)
 {
     //type REISSUABLE
-    std::vector<unsigned char> scriptVector(ParseHex("038282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
+    std::vector<unsigned char> scriptVector(ParseHex("c38282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508"));
     ColorIdentifier c1(CScript() << scriptVector);
 
     uint8_t scripthash[CSHA256::OUTPUT_SIZE];
@@ -218,10 +242,10 @@ BOOST_AUTO_TEST_CASE(coloridentifier_map_compare)
     memcpy(&c5, "048282263212c609d9ea2a6e3e172de238d8c39cabd5ac1ca10646e23fd5f51508", 33);
 
     BOOST_CHECK_EQUAL(c5 < c0, false);
-    BOOST_CHECK_EQUAL(c5 < c1, false);
-    BOOST_CHECK_EQUAL(c5 < c2, false);
-    BOOST_CHECK_EQUAL(c5 < c3, false);
-    BOOST_CHECK_EQUAL(c5 < c4, false);
+    BOOST_CHECK_EQUAL(c5 < c1, true);
+    BOOST_CHECK_EQUAL(c5 < c2, true);
+    BOOST_CHECK_EQUAL(c5 < c3, true);
+    BOOST_CHECK_EQUAL(c5 < c4, true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
