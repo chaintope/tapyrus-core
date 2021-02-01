c768058910385fbb3ff5b0e22a8fb2c38c04362f
Kohei Taniguchi <kohei@chaintope.com>
2021-02-01 13:22:44 +0900

    Add a function that issues non-reissunable colored coin to wallet test setup

--- src/wallet/test/create_transaction_tests.cpp
+++ src/wallet/test/create_transaction_tests.cpp
@@ -48,4 +48,13 @@ BOOST_FIXTURE_TEST_CASE(test_create_transaction, TestWalletSetup)
     BOOST_CHECK_EQUAL(strError, "Recipient that receives colored coin must not be a target of subtract fee");
 }
 
+BOOST_FIXTURE_TEST_CASE(test_creating_colored_transaction, TestWalletSetup)
+{
+    ImportCoin(10 * COIN);
+
+    ColorIdentifier cid;
+    BOOST_CHECK(IssueNonReissunableColoredCoin(100 * CENT, cid));
+    BOOST_CHECK_EQUAL(wallet->GetBalance()[cid], 100 * CENT);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
