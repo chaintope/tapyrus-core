c352f4201d9f4cb9949d54cc04cbbd80f16a1fda
Navia Bheeman <navia@chaintope.com>
2021-06-28 09:35:33 +0530

    fixing test failures by increasing fees

--- test/functional/wallet_coloredcoin.py
+++ test/functional/wallet_coloredcoin.py
@@ -41,6 +41,7 @@ class WalletColoredCoinTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 4
         self.setup_clean_chain = True
+        self.extra_args = [["-minrelaytxfee=0.00001"],["-minrelaytxfee=0.00001"],["-minrelaytxfee=0.00001"], []]
 
         self.pubkeys = ["025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3",
         "03831a69b8009833ab5b0326012eaf489bfea35a7321b1ca15b11d88131423fafc"]
@@ -59,10 +60,10 @@ class WalletColoredCoinTest(BitcoinTestFramework):
                         [ 1, 300],                                #level 0
                         [ 4, 267, 100, 100, 1],                   #level 1 create part1
                         [ 6, 234, 200, 100, 1, 100, 1],           #level 2 create part2
-                        [ 6, "233.9999753", 180, 90, 0, 90, 0],   #level 3 sendtoaddress
-                        [ 6, "233.9999563", 160, 80, 0, 80, 0],   #level 4 transfertoken
-                        [ 6, "233.9999441", 140, 60, 0, 60, 0],   #level 5 burn partial
-                        [ 6, "233.9999441", 140, 60, 0, 60, 0],   #level 6 burn full
+                        [ 6, "233.9999506", 180, 90, 0, 90, 0],   #level 3 sendtoaddress
+                        [ 6, "233.9999126", 160, 80, 0, 80, 0],   #level 4 transfertoken
+                        [ 6, "233.9998881", 140, 60, 0, 60, 0],   #level 5 burn partial
+                        [ 6, "233.9998881", 140, 60, 0, 60, 0],   #level 6 burn full
                       ], 
                      [ #'''nodes 1'''
                         [ 0, 0],                     #level 0
@@ -71,16 +72,16 @@ class WalletColoredCoinTest(BitcoinTestFramework):
                         [ 6, 60, 20, 10, 1, 10, 1],  #level 3
                         [ 6, 60, 40, 20, 1, 20, 1],  #level 4
                         [ 6, 60, 40, 20, 1, 20, 1],  #level 5
-                        [ 4, "59.9999798", 0, 0, 0], #level 6
+                        [ 4, "59.9999595", 0, 0, 0], #level 6
                       ],
                      [ #'''nodes 2'''
                         [ 1, 50],                 #level 0
                         [ 1, 103],                #level 1
                         [ 1, 156],                #level 2
-                        [ 1, "206.0000247"],      #level 3
-                        [ 1, "256.0000437"],      #level 4
-                        [ 1, "306.0000437"],      #level 5
-                        [ 1, "356.0000621"],      #level 6
+                        [ 1, "206.0000494"],      #level 3
+                        [ 1, "256.0000874"],      #level 4
+                        [ 1, "306.0000874"],      #level 5
+                        [ 1, "356.0001119"],      #level 6
                       ] 
         ]
 
@@ -110,9 +111,9 @@ class WalletColoredCoinTest(BitcoinTestFramework):
         connect_nodes_bi(self.nodes, 0, 2)
         self.sync_all([self.nodes[0:3]])
         relayfee = self.nodes[0].getnetworkinfo()['relayfee']
-        self.nodes[0].settxfee(relayfee)
-        self.nodes[1].settxfee(relayfee)
-        self.nodes[2].settxfee(relayfee)
+        self.nodes[0].settxfee(2 * relayfee)
+        self.nodes[1].settxfee(2 * relayfee)
+        self.nodes[2].settxfee(2 * relayfee)
 
     def test_getcolorRPC(self, utxos):
         '''test getcolor RPC using 2 different types of color addresses '''
@@ -402,7 +403,12 @@ class WalletColoredCoinTest(BitcoinTestFramework):
     def test_issuetoken(self):
 
         self.log.info("Testing issuetoken")
+        self.nodes[2].generate(20, self.signblockprivkey_wif)
+        self.sync_all([self.nodes[0:3]])
         node2_utxos = self.nodes[2].listunspent()
+
+        # Lock UTXO so nodes[2] doesn't accidentally spend it
+        self.nodes[2].lockunspent(False, [{"txid": node2_utxos[0]['txid'], "vout": node2_utxos[0]['vout']}])
 
         res1 = self.nodes[2].issuetoken(1, 100, node2_utxos[0]['scriptPubKey'])
         assert_equal(res1['color'], self.nodes[2].getcolor(1, node2_utxos[0]['scriptPubKey']))
@@ -423,8 +429,8 @@ class WalletColoredCoinTest(BitcoinTestFramework):
         self.sync_all([self.nodes[0:3]])
 
         walletinfo = self.nodes[2].getwalletinfo()
-        assert_equal(walletinfo['balance']['TPC'], decimal.Decimal('150.00005718'))
-        assert_equal(walletinfo['balance'][res1['color']], 300)
+        assert_equal(walletinfo['balance']['TPC'], decimal.Decimal('1406.00015236'))
+        assert_equal(walletinfo['balance'][res1['color']], 100)
         assert_equal(walletinfo['balance'][res2['color']], 100)
         assert_equal(walletinfo['balance'][res3['color']], 1)
 
