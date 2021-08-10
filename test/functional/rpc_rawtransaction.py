a8f06cfae889c525544107a6b954ab7fbafd03da
Navia Bheeman <navia@chaintope.com>
2021-08-10 15:46:24 +0530

    removed addwitnessaddress from tests

--- test/functional/rpc_rawtransaction.py
+++ test/functional/rpc_rawtransaction.py
@@ -43,7 +43,6 @@ class RawTransactionsTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 3
-        self.extra_args = [["-addresstype=legacy"], ["-addresstype=legacy"], ["-addresstype=legacy"]]
 
     def setup_network(self, split=False):
         super().setup_network()
