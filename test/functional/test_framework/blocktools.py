27b9a3e41ad2f0711959577de840006fb5964ea0
Navia Bheeman <navia@chaintope.com>
2021-09-24 11:19:05 +0530

    revert back removing witness utility functions t Please enter the commit message for your changes. Lines starting

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -245,3 +245,37 @@ def witness_script(use_p2wsh, pubkey):
         scripthash = sha256(witness_program)
         pkscript = CScript([OP_0, scripthash])
     return bytes_to_hex_str(pkscript)
+
+def create_witness_tx(node, use_p2wsh, utxo, pubkey, amount):
+     """Return a transaction (in hex) that spends the given utxo to a segwit output.
+
+     Optionally wrap the segwit output using P2SH."""
+     if use_p2wsh:
+         program = CScript([OP_1, hex_str_to_bytes(pubkey), OP_1, OP_CHECKMULTISIG])
+         addr = script_to_p2sh_p2wsh(program)
+     else:
+         addr = key_to_p2sh_p2wpkh(pubkey)
+     return node.createrawtransaction([utxo], {addr: amount})
+
+def send_to_witness(use_p2wsh, node, utxo, pubkey, encode_p2sh, amount, sign=True, insert_redeem_script=""):
+     """Create a transaction spending a given utxo to a segwit output.
+
+     The output corresponds to the given pubkey: use_p2wsh determines whether to
+     use P2WPKH or P2WSH; encode_p2sh determines whether to wrap in P2SH.
+     sign=True will have the given node sign the transaction.
+     insert_redeem_script will be added to the scriptSig, if given."""
+
+     scheme = random.choice(["ECDSA", "SCHNORR"])
+     tx_to_witness = create_witness_tx(node, use_p2wsh, utxo, pubkey, amount)
+     if (sign):
+         signed = node.signrawtransactionwithwallet(tx_to_witness, [], "ALL", scheme)
+         if(encode_p2sh):
+             assert("errors" not in signed or len(["errors"]) == 0)
+         return node.sendrawtransaction(signed["hex"])
+     else:
+         if (insert_redeem_script):
+             tx = FromHex(CTransaction(), tx_to_witness)
+             tx.vin[0].scriptSig += CScript([hex_str_to_bytes(insert_redeem_script)])
+             tx_to_witness = ToHex(tx)
+
+     return node.sendrawtransaction(tx_to_witness)
