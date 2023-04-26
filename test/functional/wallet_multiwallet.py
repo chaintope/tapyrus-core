fe322631eb06febe1bb6892e6ca776ffd090310b
Navia Bheeman <navia@chaintope.com>
2023-04-26 16:30:19 +0530

    change error string to check only boost::filesystem::create

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -76,7 +76,7 @@ class MultiWalletTest(BitcoinTestFramework):
             assert_equal(os.path.isfile(wallet_file(wallet_name)), True)
 
         # should not initialize if wallet path can't be created
-        exp_stderr = "boost::filesystem::create_directory: Not a directory |boost::filesystem::create_directories File exists:"
+        exp_stderr = "boost::filesystem::create"
         self.nodes[0].assert_start_raises_init_error(['-wallet=wallet.dat/bad'], exp_stderr, match=ErrorMatch.PARTIAL_REGEX)
 
         self.nodes[0].assert_start_raises_init_error(['-walletdir=wallets'], 'Error: Specified -walletdir "wallets" does not exist')
