aebecbb85a0e1671379afe66ce77ab2bc4723a2a
Navia Bheeman <navia@chaintope.com>
2023-05-09 14:33:02 +0530

    changed c++ version from 11 to 17

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -4,8 +4,8 @@ OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++
 
-darwin_CFLAGS=-pipe
-darwin_CXXFLAGS=$(darwin_CFLAGS)
+darwin_CFLAGS=-pipe -std=c11
+darwin_CXXFLAGS=-pipe -std=c++17
 
 darwin_release_CFLAGS=-O2
 darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
