17b9d57ef4f41d35385529d6ba770ad058e86803
Navia Bheeman <navia@chaintope.com>
2023-05-10 18:26:08 +0530

    remove dmg compression  (bitcoin #24031)

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -14,7 +14,7 @@ upnp_packages=miniupnpc
 darwin_native_packages = native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_libdmg-hfsplus
+darwin_native_packages += native_cctools
 endif
 
 $(host_arch)_$(host_os)_native_packages += native_b2
