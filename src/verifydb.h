471d5e2cd6e10270513deb41605426dc368d0561
Navia Bheeman <navia@chaintope.com>
2024-03-14 13:12:15 +0530

    split validation.cpp into multiple files:     validation.cpp - only transaction and block validation code     verifydb.cpp - veridydb class     chainstate.cpp - chainstate class and other helper code used in it     cs_main.cpp - common critical sections     file_io.cpp - all disk read and write operations     blockprune.cpp - blockprune class

---
+++ src/verifydb.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 Chaintope Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_VERIFYDB_H
+#define BITCOIN_VERIFYDB_H
+
+#include <coins.h>
+
+/** RAII wrapper for VerifyDB: Verify consistency of the block and coin databases */
+class CVerifyDB {
+public:
+    CVerifyDB();
+    ~CVerifyDB();
+    bool VerifyDB(CCoinsView *coinsview, int nCheckLevel, int nCheckDepth);
+};
+
+#endif //BITCOIN_VERIFYDB_H
