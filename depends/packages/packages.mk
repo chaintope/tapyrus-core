d0da95938829b8ee79cb4e6a707308d40a6bce77
Navia Bheeman <navia@chaintope.com>
2024-03-17 17:06:35 +0530

    fix macos packaging - match wih master branch

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -11,8 +11,8 @@ wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias
+darwin_native_packages =
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools
 endif
