04d1bdb1900e0793ec32b0f2b72b4e01fa788379
Navia Bheeman <navia@chaintope.com>
2024-05-09 16:37:35 +0530

    review  feedback  - move packages RPCs and  tests to  their own  files.

--- src/policy/packages.cpp
+++ src/policy/packages.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2021-2022 The Bitcoin Core developers
-// Copyright (c) 2023 Chaintope Inc.
+// Copyright (c) 2021-2023 The Bitcoin Core developers
+// Copyright (c) 2024 Chaintope Inc.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
