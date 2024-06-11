c68f8b502cbd7f95bbf5c8977c42e48435c28863
Naviabheeman <navia@chaintope.com>
2024-06-11 18:55:53 +0530

    port #13159 from bitcoin core

--- src/fs.cpp
+++ src/fs.cpp
@@ -7,9 +7,5 @@ FILE *fopen(const fs::path& p, const char *mode)
     return ::fopen(p.string().c_str(), mode);
 }
 
-FILE *freopen(const fs::path& p, const char *mode, FILE *stream)
-{
-    return ::freopen(p.string().c_str(), mode, stream);
-}
 
 } // fsbridge
