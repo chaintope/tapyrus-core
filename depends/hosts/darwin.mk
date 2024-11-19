698e52115c3031102411bb40cabc8a66f172c23a
Naviabheeman <navia@chaintope.com>
2024-11-19 11:35:34 +0530

    change msc os sdk name and version

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,6 +1,10 @@
-OSX_MIN_VERSION=10.15
-OSX_SDK_VERSION=10.15
-OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
+OSX_MIN_VERSION=13.0
+OSX_SDK_VERSION=14.0
+XCODE_VERSION=15.0
+XCODE_BUILD_ID=15A240d
+LLD_VERSION=711
+
+OSX_SDK=$(SDK_PATH)/Xcode-$(XCODE_VERSION)-$(XCODE_BUILD_ID)-extracted-SDK-with-libcxx-headers
 
 clang_prog=$(build_prefix)/bin/clang
 clangxx_prog=$(clang_prog)++
@@ -9,7 +13,7 @@ llvm_config_prog=$(build_prefix)/bin/llvm-config
 clang_resource_dir=$(build_prefix)/lib/clang/$(native_cctools_clang_version)
 
 darwin_CC=$(clang_prog) -target $(host)  -isysroot$(OSX_SDK)
-darwin_CXX=$(clangxx_prog) -target $(host) -isysroot$(OSX_SDK) -stdlib=libc++
+darwin_CXX=$(clangxx_prog) -target $(host) -isysroot$(OSX_SDK)
 
 darwin_CFLAGS=-pipe -std=c11
 darwin_CXXFLAGS=-pipe -std=c++17
