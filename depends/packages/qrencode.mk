97b6b74695a71dae8c23f6ee06308ac9580f695b
Naviabheeman <navia@chaintope.com>
2025-02-19 18:13:56 +0530

    remove boost filesystem and upgrade OSX SDK

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -17,7 +17,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_autoconf) || cat config.log
 endef
 
 define $(package)_build_cmds
