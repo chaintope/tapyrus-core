ec31c09f41511c56bbcd54d4b236fcfdb32bf59d
Naviabheeman <45412531+Naviabheeman@users.noreply.github.com>
2025-02-20 16:21:32 +0530

    ci using cmake for linux and macos x86_64 and arm64 fix reg ex comparison errors in functional tests

--- cmake/modules/FindUPNP.cmake
+++ cmake/modules/FindUPNP.cmake
@@ -1,14 +1,13 @@
-      # Try to find the UPNP - miniupnp librairy
+# Try to find the UPNP - miniupnp librairy
 # UPNP_FOUND - system has ZeroMQ lib
 # UPNP_INCLUDE_DIR - the ZeroMQ include directory
 # UPNP_LIBRARY - Libraries needed to use ZeroMQ
-      include(BrewHelper)
-      find_brew_prefix(BREW_HINT_UPNP miniupnpc)
+include(BrewHelper)
+find_brew_prefix(BREW_HINT_UPNP miniupnpc)
 
-      if(UPNP_INCLUDE_DIR AND UPNP_LIBRARY)
-          # Already in cache, be silent
-          set(UPNP_FIND_QUIETLY TRUE)
-      endif()
+if(UPNP_INCLUDE_DIR AND UPNP_LIBRARY)
+    set(UPNP_FIND_QUIETLY TRUE)
+endif()
 
 find_path(UPNP_INCLUDE_DIR NAMES miniupnpc.h HINTS ${BREW_HINT_UPNP}/include/miniupnpc)
 find_library(UPNP_LIBRARY NAMES miniupnpc libminiupnpc-dev)
