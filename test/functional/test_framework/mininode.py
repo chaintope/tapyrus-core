fe65cbf89a437fe78b77a26ec3b6b4b48016f117
Naviabheeman <45412531+Naviabheeman@users.noreply.github.com>
2025-02-26 11:25:35 +0530

    more start timeout

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -29,7 +29,7 @@ logger = logging.getLogger("TestFramework.mininode")
 
 # all timeout
 TOTAL_TEST_DURATION = 60 * 60 #one test can't run more than 60 min
-TAPYRUSD_PROC_TIMEOUT = 360 # time to wait for tapyrusd to start
+TAPYRUSD_PROC_TIMEOUT = 600 # time to wait for tapyrusd to start
 TAPYRUSD_MESSAGE_TIMEOUT = 60 # time to wait for a response from tapyrusd
 TAPYRUSD_MIN_TIMEOUT = 5 # a minimumtime to wait
 TAPYRUSD_SYNC_TIMEOUT = 900 # time for multiple nodes to sync
