c39edcf008bd634b97036ad8a78e8e074ef4bca1
Naviabheeman <navia@chaintope.com>
2025-06-09 22:00:34 +0530

    match benchmark test data to autotools

--- cmake/script/GenerateHeaderFromRaw.cmake
+++ cmake/script/GenerateHeaderFromRaw.cmake
@@ -6,18 +6,11 @@ cmake_path(GET RAW_SOURCE_PATH STEM raw_source_basename)
 
 file(READ ${RAW_SOURCE_PATH} hex_content HEX)
 string(REGEX REPLACE "................" "\\0\n" formatted_bytes "${hex_content}")
-string(REGEX REPLACE "[^\n][^\n]" "std::byte{0x\\0}," formatted_bytes "${formatted_bytes}")
+string(REGEX REPLACE "[^\n][^\n]" "0x\\0, " formatted_bytes "${formatted_bytes}")
 
 set(header_content
-"#include <cstddef>
-#include <span>
-
-namespace ${RAW_NAMESPACE} {
-static unsigned const char ${json_source_basename}[] {
+"static unsigned const char ${raw_source_basename}[] = {
 ${formatted_bytes}
 };
-
-//inline constexpr std::span ${raw_source_basename}{detail_${raw_source_basename}_raw};
-//}
 ")
 file(WRITE ${HEADER_PATH} "${header_content}")
