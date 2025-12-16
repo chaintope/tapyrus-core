83a4a009ee355a985d7fd72251d77068670b6d27
Naviabheeman <45412531+Naviabheeman@users.noreply.github.com>
2025-12-16 15:36:37 +0530

    port prevector class from bitcoin core. remove reverse_iterator and its uses to compile prevector changes successfully

--- src/test/prevector_tests.cpp
+++ src/test/prevector_tests.cpp
@@ -6,13 +6,13 @@
 #include <vector>
 #include <prevector.h>
 
-#include <reverse_iterator.h>
 #include <serialize.h>
 #include <streams.h>
 
 #include <test/test_tapyrus.h>
 
 #include <boost/test/unit_test.hpp>
+#include <ranges>
 
 BOOST_FIXTURE_TEST_SUITE(prevector_tests, TestingSetup)
 
@@ -58,13 +58,13 @@ class prevector_tester {
         for (const T& v : pre_vector) {
              local_check(v == real_vector[pos++]);
         }
-        for (const T& v : reverse_iterate(pre_vector)) {
+        for (const T& v : std::views::reverse(pre_vector)) {
              local_check(v == real_vector[--pos]);
         }
         for (const T& v : const_pre_vector) {
              local_check(v == real_vector[pos++]);
         }
-        for (const T& v : reverse_iterate(const_pre_vector)) {
+        for (const T& v : std::views::reverse(const_pre_vector)) {
              local_check(v == real_vector[--pos]);
         }
         CDataStream ss1(SER_DISK, 0);
