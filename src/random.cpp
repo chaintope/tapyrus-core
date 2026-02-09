5b1f9ec9935921591099e530c7fc63d77a768861
Naviabheeman <45412531+Naviabheeman@users.noreply.github.com>
2026-02-09 19:22:35 +0530

    change HAVE_SYS_GETRANDOM to HAVE_GETRANDOM

--- src/random.cpp
+++ src/random.cpp
@@ -29,15 +29,11 @@
 #include <sys/time.h>
 #endif
 
-#ifdef HAVE_SYS_GETRANDOM
-#include <sys/syscall.h>
-#include <linux/random.h>
-#endif
-#if defined(HAVE_GETENTROPY) || defined(HAVE_GETENTROPY_RAND)
-#include <unistd.h>
-#endif
-#if defined(HAVE_GETENTROPY_RAND) && defined(__APPLE__)
+#if defined(HAVE_GETRANDOM) || defined(HAVE_GETENTROPY_RAND)
 #include <sys/random.h>
+#endif
+#if defined(HAVE_GETENTROPY)
+#include <unistd.h>
 #endif
 #ifdef HAVE_SYSCTL_ARND
 #include <utilstrencodings.h> // for ARRAYLEN
@@ -286,23 +282,14 @@ void GetOSRand(unsigned char *ent32)
         RandFailure();
     }
     CryptReleaseContext(hProvider, 0);
-#elif defined(HAVE_SYS_GETRANDOM)
+#elif defined(HAVE_GETRANDOM)
     /* Linux. From the getrandom(2) man page:
      * "If the urandom source has been initialized, reads of up to 256 bytes
      * will always return as many bytes as requested and will not be
      * interrupted by signals."
      */
-    int rv = syscall(SYS_getrandom, ent32, NUM_OS_RANDOM_BYTES, 0);
-    if (rv != NUM_OS_RANDOM_BYTES) {
-        if (rv < 0 && errno == ENOSYS) {
-            /* Fallback for kernel <3.17: the return value will be -1 and errno
-             * ENOSYS if the syscall is not available, in that case fall back
-             * to /dev/urandom.
-             */
-            GetDevURandom(ent32);
-        } else {
-            RandFailure();
-        }
+    if (getrandom(ent32, NUM_OS_RANDOM_BYTES, 0) != NUM_OS_RANDOM_BYTES) {
+        RandFailure();
     }
 #elif defined(HAVE_GETENTROPY) && defined(__OpenBSD__)
     /* On OpenBSD this can return up to 256 bytes of entropy, will return an
